52d40c2c73250f4762f4503ffb0d6e5efae8e752
Fabian Zwissler <fabian.zwissler@bosch.com>
2023-05-11 11:50:45 +0200

    animierter seat

--- distance_alarm.js
+++ distance_alarm.js
@@ -1,9 +1,10 @@
 const plugin = ({ widgets, vehicle, simulator }) => {
     const container = document.createElement("div");
-    container.setAttribute("style", `display:flex; align-items:center; justify-content:center; height: 100%; width: 100%;`);
+    container.setAttribute("style", `align-items:center; justify-content:center; height: 100%; width: 100%;`);
     container.innerHTML = `
     <div class="distance">
         <input type="checkbox" id="distanceIndicator" name="distanceIndicator" value="Too Close!">
+		<img src="seatOff.png" id="mySeat">
     </div>
       `;
 
@@ -24,7 +25,16 @@ const plugin = ({ widgets, vehicle, simulator }) => {
     console.log("Element: " + alarm.innerHTML);
 
     function setAlarmState(value){
-        alarm.checked = value;
+		alarm.checked = value;
+	
+		if (value)
+		{
+			alarm.setAttribute("src", "seatOn.gif")
+		}
+		else
+		{
+			alarm.setAttribute("src", "seatOff.gif")
+		}
     }
     
 
