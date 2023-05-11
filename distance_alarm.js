b2bef1735c86a5f215b6400410a2228b9a34990c
Fabian Zwissler <fabian.zwissler@bosch.com>
2023-05-11 12:17:08 +0200

    animierter seat

--- distance_alarm.js
+++ distance_alarm.js
@@ -1,51 +1,21 @@
 const plugin = ({ widgets, vehicle, simulator }) => {
-    const container = document.createElement("div");
-    container.setAttribute("style", `align-items:center; justify-content:center; height: 100%; width: 100%;`);
-    container.innerHTML = `
-    <div class="distance">
-        <input type="checkbox" id="distanceIndicator" name="distanceIndicator" value="Too Close!">
-		<img src="seatOff.png" id="mySeat">
-    </div>
-      `;
-
-    let boxGlobal = null;
-    const listeners = [];
-
-    widgets.register("DistanceAlarm", (box) => {
-        boxGlobal = box;
-        box.injectNode(container);
-        return () => {
-            boxGlobal = null;
-            // Deactivation function for clearing intervals or such.
-        };
-    });
-
-    var alarm = container.getElementsByClassName("distance")[0].children[0]
-    console.log("Element getter");
-    console.log("Element: " + alarm.innerHTML);
-
-    function setAlarmState(value){
-		alarm.checked = value;
-	
-		if (value)
-		{
-			alarm.setAttribute("src", "seatOn.png")
-		}
-		else
-		{
-			alarm.setAttribute("src", "seatOff.gif")
-		}
-    }
     
+	
+	widgets.register(
+		"SeatAnimated",
+		SignalWithMedia("Vehicle.Driver.Announcement.Haptic.Seat.Seating.Vibrating", {
+			[false]: {
+				type: "image",
+				url: "https://fazwit00.netlify.app/SeatOff.png"
+			},
+			[true]: {
+				type: "image",
+				url: "https://fazwit00.netlify.app/SeatOn.png"
+			}
+		}, vehicle)
+	)
 
-    simulator("Vehicle.Driver.Announcement.Haptic.Seat.Seating.Vibrating", "set", async ({ args }) => {
-        console.log("called set");
-        console.log("called set with: " +args[0]);
-        
-        const value = args[0];
-        setAlarmState(value)
-    });
-
+    }
 
     return {};
 };
